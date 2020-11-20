2a972d5e79fa192a35127da24d5bbc471d941cd7
Emmanuel Ponce <jesus.ponce.supernova@gmail.com>
2020-11-19 22:05:33 -0600

    first config and input to search

---
+++ src/app/shared/components/form-search/form-search.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-form-search',
+  template: `
+    <input
+      #inputSearch
+      autofocus
+      type="text"
+      class="form-control-lg"
+      placeholder="Buscar"
+      (keyup)="onSearch(inputSearch.value)"
+    >
+  `,
+  styleUrls: []
+})
+export class FormSearchComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSearch(value: string){
+    console.log(value)
+    if (value && value.length > 3) {
+      this.router.navigate(['/character-list'], {
+        queryParams: {
+          q: value
+        }
+      })
+    } else {
+
+    }
+  }
+}
